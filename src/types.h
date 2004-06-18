d4de6b2710dd2512a3a015c5d983432f7d3fd0f5
Daniel Adler <dadler@uni-goettingen.de>
2004-06-18 00:00:00 +0000

    version 0.64-12

--- src/types.h
+++ src/types.h
@@ -5,8 +5,10 @@
 // C++ header file
 // This file is part of RGL
 //
-// $Id: types.h,v 1.3 2003/11/27 21:12:23 dadler Exp $
+// $Id: types.h,v 1.5 2004/05/28 11:00:48 dadler Exp $
 
+
+#include <cstring>
 
 //
 //
--- src/win32gui.h
+++ src/win32gui.h
@@ -4,7 +4,7 @@
 // C++ header file
 // This file is part of RGL
 //
-// $Id: win32gui.h,v 1.1.1.1 2003/03/25 00:13:21 dadler Exp $
+// $Id: win32gui.h,v 1.3 2004/05/28 12:06:31 dadler Exp $
 
 
 #include "gui.h"
@@ -19,13 +19,13 @@ namespace gui {
   public:
   
     Win32GUIFactory(HINSTANCE inModuleHandle);
-    ~Win32GUIFactory();
+    virtual ~Win32GUIFactory();
   
     WindowImpl* createWindowImpl(Window* window);
       
   };
 
-};
+} // namespace gui
 
 
 #endif /* WIN32_GUI_H */
